4eb05c9e6fc1a16290465cacef7f6a94d1eaba84
survivorbat <djbatcat@gmail.com>
2020-01-24 22:44:33 +0100

    master: Update readme and deployment

--- terraform/kubernetes_clusters.tf
+++ terraform/kubernetes_clusters.tf
@@ -10,14 +10,6 @@ resource "digitalocean_kubernetes_cluster" "main" {
   }
 }
 
-provider "kubernetes" {
-  host  = digitalocean_kubernetes_cluster.main.endpoint
-  token = digitalocean_kubernetes_cluster.main.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-  digitalocean_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
-  )
-}
-
 resource "kubernetes_namespace" "portfolio_namespace" {
   provider = kubernetes
   metadata {
