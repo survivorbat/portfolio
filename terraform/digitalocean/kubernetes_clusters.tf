f7f20f2a5f36b76a9eee147f867a7d8e5398a897
survivorbat <djbatcat@gmail.com>
2020-01-25 09:38:35 +0100

    master: Remove terraform files

--- terraform/digitalocean/kubernetes_clusters.tf
+++ terraform/digitalocean/kubernetes_clusters.tf
@@ -1,18 +1,18 @@
-//resource "digitalocean_kubernetes_cluster" "main" {
-//  name    = "projects"
-//  region  = "nyc1"
-//  version = "1.16.2-do.3"
-//  tags = ["production"]
-//  node_pool {
-//    name       = "main-pool"
-//    node_count = 1
-//    size       = "s-2vcpu-2gb"
-//  }
-//}
-//
-//resource "kubernetes_namespace" "portfolio_namespace" {
-//  provider = kubernetes
-//  metadata {
-//    name = "portfolio"
-//  }
-//}
+resource "digitalocean_kubernetes_cluster" "main" {
+  name    = "projects"
+  region  = "nyc1"
+  version = "1.16.2-do.3"
+  tags = ["production"]
+  node_pool {
+    name       = "main-pool"
+    node_count = 1
+    size       = "s-2vcpu-2gb"
+  }
+}
+
+resource "kubernetes_namespace" "portfolio_namespace" {
+  provider = kubernetes
+  metadata {
+    name = "portfolio"
+  }
+}
