f40bc3babf10e030f8659eeb56f71cef4cab3fe7
survivorbat <djbatcat@gmail.com>
2020-01-27 22:19:05 +0100

    masteR: Update firewalls and deploy scripts

--- common/terraform/digitalocean/firewalls.tf
+++ common/terraform/digitalocean/firewalls.tf
@@ -17,5 +17,29 @@ resource "digitalocean_firewall" "default_firewall" {
     port_range = "22"
   }
 
+  outbound_rule {
+    protocol = "tcp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+    port_range = "80"
+  }
+
+  outbound_rule {
+    protocol = "tcp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+    port_range = "443"
+  }
+
+  outbound_rule {
+    protocol = "tcp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+    port_range = "53"
+  }
+
+  outbound_rule {
+    protocol = "udp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+    port_range = "53"
+  }
+
   droplet_ids = [digitalocean_droplet.portfolio_droplet.id]
 }
