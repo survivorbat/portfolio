715c848c12e4a68f1efef58248508a6f9ea439e1
survivorbat <djbatcat@gmail.com>
2020-04-12 20:51:45 +0200

    master: Update scripts

--- common/terraform/variables.tf
+++ common/terraform/variables.tf
@@ -1,11 +1,14 @@
 variable "do_token" {
   description = "Token to connect to DigitalOcean"
+  default = "__doToken__"
 }
 
 variable "personal_public_key" {
   description = "Path to personal public key"
+  default = "__personalPublicKey__"
 }
 
 variable "portfolio_public_key" {
   description = "Path to portfolio public key"
+  default = "__portfolioPublicKey__"
 }
