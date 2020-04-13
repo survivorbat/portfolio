baa524c27adc43b06575ea40d3cd194cd615419d
survivorbat <djbatcat@gmail.com>
2020-04-13 20:12:39 +0200

    master: Re-add backend values and reset state

--- common/terraform/backend.tf
+++ common/terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name = "Portfolio"
-    storage_account_name = "__storageAccountName__"
-    container_name = "__containerName__"
+    storage_account_name = "portfoliostate"
+    container_name = "tfstate"
     key = "prod.terraform.tfstate"
   }
 }
