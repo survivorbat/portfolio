cceb12b30fde767a3cb59bde4f4c260345d7a57d
survivorbat <djbatcat@gmail.com>
2020-04-13 12:49:58 +0200

    master: Remove terraform docker image

--- common/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "Portfolio"
-    storage_account_name = "__storageAccountName__"
-    container_name = "__containerName__"
-    key = "prod.terraform.tfstate"
-  }
-}
