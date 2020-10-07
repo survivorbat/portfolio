9366f4341444de122746cece22f36a87b3392ecd
survivorbat <djbatcat@gmail.com>
2020-10-07 20:28:27 +0200

    Add certificate generation in pipeline

--- common/terraform/main.tf
+++ common/terraform/main.tf
@@ -68,3 +68,45 @@ resource "digitalocean_ssh_key" "public_key" {
   name = "portfolio_key"
   public_key = var.portfolio_public_key
 }
+
+provider "acme" {
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+}
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits = "2048"
+}
+
+resource "acme_registration" "acme_registration" {
+  account_key_pem = tls_private_key.private_key.private_key_pem
+  email_address = "" # TODO
+}
+
+resource "acme_certificate" "acme_cert" {
+  account_key_pem = acme_registration.acme_registration.account_key_pem
+  dns_challenge {
+    provider = "digitalocean"
+    config = {
+      DO_AUTH_TOKEN = var.do_token
+    }
+  }
+
+  subject_alternative_names = [
+    "*.maarten.dev",
+    "maartenvanderheijden.dev",
+    "*.maartenvanderheijden.dev",
+  ]
+  common_name = "maarten.dev"
+}
+
+# Dump the key and certificate in the pipeline
+resource "local_file" "acme_fullchain" {
+  filename = "fullchain.pem"
+  sensitive_content = acme_certificate.acme_cert.certificate_pem
+}
+
+resource "local_file" "acme_private_key" {
+  filename = "key.pem"
+  sensitive_content = acme_certificate.acme_cert.private_key_pem
+}
