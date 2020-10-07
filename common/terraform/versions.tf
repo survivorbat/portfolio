64a23e5f9e0d22be53807aa293cc969e8a48f457
survivorbat <djbatcat@gmail.com>
2020-10-07 20:45:11 +0200

    Update ssl certificates

--- common/terraform/versions.tf
+++ common/terraform/versions.tf
@@ -3,6 +3,10 @@ terraform {
     digitalocean = {
       source = "terraform-providers/digitalocean"
     }
+
+    acme = {
+      source = "terraform-providers/acme"
+    }
   }
   required_version = ">= 0.13"
 }
