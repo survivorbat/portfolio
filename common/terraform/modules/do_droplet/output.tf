d8421175ba35307a7cf472a34684208607322b8d
survivorbat <djbatcat@gmail.com>
2020-10-26 10:22:46 +0100

    Remove outputs

--- common/terraform/modules/do_droplet/output.tf
+++ common/terraform/modules/do_droplet/output.tf
@@ -2,16 +2,8 @@ output "ipv4" {
   value = digitalocean_droplet.droplet.ipv4_address
 }
 
-output "ipv4_private" {
-  value = digitalocean_droplet.droplet.ipv4_address_private
-}
-
 output "ipv6" {
   value = digitalocean_droplet.droplet.ipv6_address
-}
-
-output "ipv6_private" {
-  value = digitalocean_droplet.droplet.ipv6_address_private
 }
 
 output "droplet_urn" {
